2f1e38b0871fa7bfe4687a2ac51c0d9f2c3f1b02
carrotflakes <carrotflakes@gmail.com>
2022-03-12 19:48:00 +0900

    functions::Sum

--- src/functions.rs
+++ src/functions.rs
@@ -38,3 +38,48 @@ impl Function for Exp {
         vec![Variable::new(*gys[0] * xs[0].exp())]
     }
 }
+
+pub struct Sum;
+
+impl Function for Sum {
+    fn forward(&self, xs: &Vec<Variable>) -> Vec<Variable> {
+        assert!(xs.len() >= 1);
+        let mut y = *xs[0].clone();
+        for x in xs.iter().skip(1) {
+            y += **x;
+        }
+        vec![Variable::new(y)]
+    }
+
+    fn backward(&self, xs: &Vec<Variable>, gys: &Vec<Variable>) -> Vec<Variable> {
+        (0..xs.len()).map(|_| gys[0].clone()).collect()
+    }
+}
+
+#[test]
+fn test_sum() {
+    {
+        let x = Variable::new(1.0);
+        let y = Variable::new(2.0);
+        let z = Variable::new(3.0);
+        let xs = vec![x.clone(), y.clone(), z.clone()];
+        let ys = Sum.call(xs);
+        assert_eq!(*ys[0], 6.0);
+
+        ys[0].set_grad(Variable::new(1.0));
+        ys[0].backward();
+        assert_eq!(*x.get_grad().unwrap(), 1.0);
+        assert_eq!(*y.get_grad().unwrap(), 1.0);
+        assert_eq!(*z.get_grad().unwrap(), 1.0);
+    }
+    {
+        let x = Variable::new(3.0);
+        Sum.call(vec![x.clone(), x.clone()]);
+        let ys = Sum.call(vec![x.clone(), x.clone()]);
+        assert_eq!(*ys[0], 6.0);
+
+        ys[0].set_grad(Variable::new(1.0));
+        ys[0].backward();
+        assert_eq!(*x.get_grad().unwrap(), 2.0);
+    }
+}
