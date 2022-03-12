730f39d09884f993f934fa1a33d0543b182980df
carrotflakes <carrotflakes@gmail.com>
2022-03-13 02:38:39 +0900

    test_rosenbrock_sgd

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,6 @@
 use crate::{
     functions::{Mul, Pow, Sub, Sum},
-    Function, Variable,
+    Function, Variable, release_variables,
 };
 
 #[test]
@@ -66,41 +66,66 @@ fn test_matyas() {
     assert!((y.get_grad().unwrap().get_data()[0] - 0.04).abs() < 1e-6);
 }
 
-#[test]
-fn test_rosenbrock() {
-    let a = Variable::new(0.0.into());
-    let b = Variable::new(2.0.into());
-
-    let y = Sum
-        .call(vec![
-            Mul.call(vec![
-                Variable::new(100.0.into()),
-                Pow::new(2.0)
-                    .call(vec![Sub
-                        .call(vec![
-                            b.clone(),
-                            Pow::new(2.0).call(vec![a.clone()]).pop().unwrap(),
-                        ])
-                        .pop()
-                        .unwrap()])
-                    .pop()
-                    .unwrap(),
-            ])
-            .pop()
-            .unwrap(),
+fn rosenbrock(a: Variable, b: Variable) -> Variable {
+    Sum.call(vec![
+        Mul.call(vec![
+            Variable::new(100.0.into()),
             Pow::new(2.0)
                 .call(vec![Sub
-                    .call(vec![a.clone(), Variable::new(1.0.into())])
+                    .call(vec![
+                        b.clone(),
+                        Pow::new(2.0).call(vec![a.clone()]).pop().unwrap(),
+                    ])
                     .pop()
                     .unwrap()])
                 .pop()
                 .unwrap(),
         ])
         .pop()
-        .unwrap();
+        .unwrap(),
+        Pow::new(2.0)
+            .call(vec![Sub
+                .call(vec![a.clone(), Variable::new(1.0.into())])
+                .pop()
+                .unwrap()])
+            .pop()
+            .unwrap(),
+    ])
+    .pop()
+    .unwrap()
+}
+
+#[test]
+fn test_rosenbrock() {
+    let a = Variable::new(0.0.into());
+    let b = Variable::new(2.0.into());
+
+    let y = rosenbrock(a.clone(), b.clone());
 
     y.set_grad(Variable::new(1.0.into()));
     y.backward();
     assert!((a.get_grad().unwrap().get_data()[0] - -2.0).abs() < 1e-6);
     assert!((b.get_grad().unwrap().get_data()[0] - 400.0).abs() < 1e-6);
+}
+
+#[test]
+fn test_rosenbrock_sgd() {
+    let mut a = Variable::new(0.0.into());
+    let mut b = Variable::new(2.0.into());
+    let lr = 0.001;
+
+    for _ in 0..50000 {
+        // dbg!((a.get_data()[0], b.get_data()[0]));
+        let y = rosenbrock(a.clone(), b.clone());
+        
+        y.set_grad(Variable::new(1.0.into()));
+        y.backward();
+
+        a = Variable::new((a.data[0] - lr * a.get_grad().unwrap().data[0]).into());
+        b = Variable::new((b.data[0] - lr * b.get_grad().unwrap().data[0]).into());
+        release_variables(&y);
+    }
+
+    assert!((a.get_data()[0] - 1.0).abs() < 1e-3);
+    assert!((b.get_data()[0] - 1.0).abs() < 1e-3);
 }
