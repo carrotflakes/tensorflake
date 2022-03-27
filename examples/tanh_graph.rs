8b88451ae4c2ae83f7b6783320616bdae21bc14b
carrotflakes <carrotflakes@gmail.com>
2022-03-27 12:25:51 +0900

    refactor tanh_graph.rs

--- examples/tanh_graph.rs
+++ examples/tanh_graph.rs
@@ -4,19 +4,17 @@ dot tanh_2.dot -T png -o tanh_2.png
 dot tanh_3.dot -T png -o tanh_3.png
  */
 
-use tensorflake::{functions::*, *};
+use tensorflake::*;
 
 fn main() {
     let x = backprop(scalar(1.0)).named("x");
-    let y = call!(Tanh, x).named("y");
+    let y = x.tanh().named("y");
 
     let mut gs = gradients(&[y.clone()], &[x.clone()], true);
 
     for i in 1..=3 {
-        let gx = gs[0]
-            .clone()
-            .named(format!("gx{}", i));
-            
+        let gx = gs[0].clone().named(format!("gx{}", i));
+
         gs = gradients(&gs, &[x.clone()], true);
 
         export_dot::export_dot(&[gx], &format!("tanh_{}.dot", i)).unwrap();
