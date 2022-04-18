ac66e08b39ee12cd92fc9ec82d4342bf00e2f176
carrotflakes <carrotflakes@gmail.com>
2022-04-19 00:25:56 +0900

    gelu

--- src/nn/activations/mod.rs
+++ src/nn/activations/mod.rs
@@ -1,8 +1,10 @@
+mod gelu;
 mod relu;
 mod sigmoid;
 mod softmax;
 
 pub use crate::functions::tanh;
+pub use gelu::gelu;
 pub use relu::{relu, Relu};
 pub use sigmoid::{naive_sigmoid, sigmoid, Sigmoid};
 pub use softmax::{softmax, Softmax};
